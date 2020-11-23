7342f063d530f2423eefcc22cdddf4af0e0e1f92
Viktor <giftiheanacho54@gmail.com>
2020-11-23 02:54:37 +0000

    making a commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # amazon-clone
 creating amazon clone with react,hook, redux and firebase
+* set up the react app with { npx create-react-app amazon }
+* set up firebase
+* set up react router
---
+++ amazon/src/Header.js
@@ -0,0 +1,50 @@
+// rfce for short
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket"
+
+function Header() {
+    return <nav className = "header">
+            <Link to = "/">
+                <img className = "header_logo" src = "http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt=""></img>
+            </Link>
+        
+            <div className = "header_Search">
+                <input type = "text" className = "header_searchInput"/>
+                <SearchIcon className = "header_searchIcon"/>
+            </div>
+            
+            <div className = "header_nav">
+                <Link to = "/login" className = "header_Link">
+                    <div className= "header_option">
+                        <span className = "header_optionLineOne">Hello viktor</span>
+                        <span className = "header_optionLineTwo">Sign In</span>
+                    </div>
+                </Link>
+            
+                <Link to = "/" className = "header_Link">
+                    <div className= "header_option">
+                        <span className = "header_optionLineOne">& Orders</span>
+                        <span className = "header_optionLineTwo">Returns</span>
+                    </div>
+                </Link>
+                <Link to = "/login" className = "header_Link">
+                    <div className= "header_option">
+                        <span className = "header_optionLineOne">Your</span>
+                        <span className = "header_optionLineTwo">Prime</span>
+                    </div>
+                </Link>
+            </div>
+            
+            <Link to = "/checkout">
+                <div className = "header_optionBasket">
+                    <ShoppingBasketIcon />
+                    <span className = "header_optionLineTwo header_basketCount">0</span>
+                </div>
+            </Link>      
+    </nav>
+}
+
+export default Header
